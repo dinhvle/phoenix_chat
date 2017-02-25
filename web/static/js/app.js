57108684771d0c1ed5922997cd671f58c45923d3
Dinh Le <dinh.v.le@gmail.com>
2017-02-25 15:09:28 -0600

    add and implement phoenix presence to track users online

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,6 +12,52 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
+import {Socket, Presence} from "phoenix"
+
+// Socket
+let user = document.getElementById("User").innerText
+let socket = new Socket("/socket", {params: {user: user}})
+socket.connect()
+
+// Presence
+let presences = {}
+
+let formatTimestamp = (timestamp) => {
+  let date = new Date(timestamp)
+  return date.toLocaleTimeString()
+}
+let listBy = (user, {metas: metas}) => {
+  return {
+    user: user,
+    onlineAt: formatTimestamp(metas[0].online_at)
+  }
+}
+
+let userList = document.getElementById("UserList")
+let render = (presences) => {
+  userList.innerHTML = Presence.list(presences, listBy)
+    .map(presence => `
+      <li>
+        <b>${presence.user}</b>
+        <br><small>online since ${presence.onlineAt}</small>
+      </li>
+    `)
+    .join("")
+}
+
+// Channels
+let room = socket.channel("room:lobby", {})
+room.on("presence_state", state => {
+  presences = Presence.syncState(presences, state)
+  render(presences)
+})
+
+room.on("presence_diff", diff => {
+  presences = Presence.syncDiff(presences, diff)
+  render(presences)
+})
+
+room.join()
 
 // Import local files
 //
